666e1898784127eb49ca005f66966c635af3b9df
PhasitWo <vrzoearth@gmail.com>
2023-10-18 22:51:30 +0700

    lab6 refactor

--- Algo/lab6/lab6.py
+++ Algo/lab6/lab6.py
@@ -20,19 +20,26 @@ def readInput(filePath:str) -> list[list[int]]:
     return testCases
 
 def solve_scc(g:Graph) -> int:
-    scc_lst = []
-    def DFS(g:Graph):
-        for x in g.vertices:
-            x.color = Color.WHITE
-            x.predecessor = None
-        time = 0
-        # sort by f time
-        g.vertices.sort(key=lambda x: x.f, reverse=True)
-        for u in g.vertices:
-            if u.color == Color.WHITE:
-                time = DFS_visit(g, u, time)
-                scc_lst.append(extract_scc(g))
-    def DFS_visit(g:Graph, u:Vertex, time):
+    DFS(g)
+    g_t = g.transpose()
+    scc_lst = DFS(g_t)
+    return scc_lst
+
+def DFS(g:Graph):
+    trees = []
+    for x in g.vertices:
+        x.color = Color.WHITE
+        x.predecessor = None
+    time = 0
+    # sort by f time
+    g.vertices.sort(key=lambda x: x.f, reverse=True)
+    for u in g.vertices:
+        if u.color == Color.WHITE:
+            time = DFS_visit(g, u, time)
+            trees.append(extract_scc(g))
+    return trees
+
+def DFS_visit(g:Graph, u:Vertex, time):
         time += 1
         u.d = time
         u.color = Color.GRAY
@@ -45,13 +52,6 @@ def solve_scc(g:Graph) -> int:
         u.f = time
         return time
     
-    # Solve
-    DFS(g)
-    g_t = g.transpose()
-    scc_lst = []
-    DFS(g_t)
-    return scc_lst
-
 def extract_scc(g:Graph):
     lst = []
     for v in g.vertices:
@@ -60,21 +60,27 @@ def extract_scc(g:Graph):
             v.color = Color.RED
     return lst
 
-
-
+def unify(g:Graph):
+    pass
+    # interpret each ssc as a vertex
+    # find relations (edges)
+    # do topoligical sorting
+    # connect least f to max f
 
 # Driver Code
 testcases = readInput("Algo/lab6/example.txt")
-for graph in testcases:
-    SCCs = solve_scc(graph)
-    res = "result: "
-    if len(SCCs) > 1:
-        res += "0"
-    else:
-        res += "1"
-    print(res)
-    print("SCCs:")
-    for s in SCCs:
-        print(s)
+for c in testcases:
+    unify(c)
+# for graph in testcases:
+#     SCCs = solve_scc(graph)
+#     res = "result: "
+#     if len(SCCs) > 1:
+#         res += "0"
+#     else:
+#         res += "1"
+#     print(res)
+#     print("SCCs:")
+#     for s in SCCs:
+#         print(s)
  
 
