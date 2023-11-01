7eda2193c86bb81f9ab70fd00e543ca159491789
PhasitWo <vrzoearth@gmail.com>
2023-11-01 23:39:58 +0700

    algo lab8 non dac itr2

--- Algo/lab8/idea.py
+++ Algo/lab8/idea.py
@@ -1,36 +1,60 @@
 def notDAC(arr:list[int]):
     new = arr.copy()
     for i in range(len(arr)):
-        if check(new, i) == False:
-            new = rearrange(new) 
+        type_ = 0 if i % 2 == 0 else 1
+        rearrange(arr, new, i, type_)
     return new
+def rearrange(old:list, new:list, i, type_):
+    x2 = i
+    x1 = x2-1
+    x3 = x2+1
+    l_candidate = None
+    r_candidate = None
+    while(x1>-1 and x3<len(old)):
+        x1_index = new.index(x1)
+        x3_index = new.index(x3)
+        if x1_index < i and i < x3_index:
+            l_candidate = x1_index
+            r_candidate = x3_index
+        if x3_index < i and i < x1_index:
+            l_candidate = x3_index
+            r_candidate = x1_index
+        x1 -= 1
+        x3 += 1
+    if l_candidate != None and r_candidate != None:
+        x2_index = new.index(x2)
+        if type_ == 0:
+            temp = new[l_candidate]
+            new[l_candidate] = x2
+            new[x2_index] = temp
+        else:
+            temp = new[r_candidate]
+            new[r_candidate] = x2
+            new[x2_index] = temp
 
-def rearrange(arr):
-    new = []
-    new += arr[0::2]
-    new += arr[1::2]
-    return new
 
-def check(arr, num_index:int) -> bool:
-    num = arr[num_index]
-    l_num = num-1
-    r_num = num+1
-    flag = True
-    while(l_num>-1 and r_num<len(arr)):
-        left_index = arr.index(l_num)
-        right_index = arr.index(r_num)
-        if left_index < num_index and num_index < right_index:
-            flag = False
-            break
-        if right_index < num_index and num_index < left_index:
-            flag = False
-            break
-        l_num -= 1
-        r_num += 1
-    if not flag:
-        return False
-    return True
+def check(arr:list[int]):
+    for num_index, num in enumerate(arr):
+        l_num = num-1
+        r_num = num+1
+        flag = True
+        while(l_num>-1 and r_num<len(arr)):
+            left_index = arr.index(l_num)
+            right_index = arr.index(r_num)
+            if left_index < num_index and num_index < right_index:
+                flag = False
+                break
+            if right_index < num_index and num_index < left_index:
+                flag = False
+                break
+            l_num -= 1
+            r_num += 1
+        if not flag:
+            print("INCORRECT", num)
+            return
+    print("CORRECT!!!")
 
 q = [i for i in range(6)]
-print(q)
-print(notDAC(q))
+new = notDAC(q)
+print(new)
+check(new)
--- Algo/lab8/test.py
+++ Algo/lab8/test.py
@@ -1,2 +1,3 @@
-x = 3.6
-print(int(x))
+x= [0,1,2,3,4]
+x.insert(3, x.pop(2))
+print(x)
