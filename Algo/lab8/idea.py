1b069976ee7584c49b6e7a79a2309d41010b7880
PhasitWo <vrzoearth@gmail.com>
2023-11-01 22:56:00 +0700

    algo lab8 non dac idea

--- Algo/lab8/idea.py
+++ Algo/lab8/idea.py
@@ -1,51 +1,36 @@
-def recur(arr:list[int], p, r):
-    if r-p > 2:
-        q = split(arr, p, r)
-        recur(arr, p, q) # left = items that index are even
-        recur(arr, q+1, r) # right = items that index are odd
-    return arr
+def notDAC(arr:list[int]):
+    new = arr.copy()
+    for i in range(len(arr)):
+        if check(new, i) == False:
+            new = rearrange(new) 
+    return new
 
-def split(arr:list[int], p, r):
-    q = int((r+p-1)/2)
-    even_index = r
-    if r % 2 != 0:
-        even_index -= 1
-    for odd_index in range(1, q+1, 2):
-        # swap
-        temp = arr[odd_index]
-        arr[odd_index] = arr[even_index]
-        arr[even_index] = temp
-        even_index -= 2
-    return q
+def rearrange(arr):
+    new = []
+    new += arr[0::2]
+    new += arr[1::2]
+    return new
 
-def check(arr:list[int]):
-    for num_index, num in enumerate(arr):
-        l_num = num-1
-        r_num = num+1
-        flag = True
-        while(l_num>-1 and r_num<len(arr)):
-            left_index = arr.index(l_num)
-            right_index = arr.index(r_num)
-            if left_index < num_index and num_index < right_index:
-                flag = False
-                break
-            if right_index < num_index and num_index < left_index:
-                flag = False
-                break
-            l_num -= 1
-            r_num += 1
-        if not flag:
-            print("INCORRECT", num)
-            return
-    print("CORRECT!!!")
+def check(arr, num_index:int) -> bool:
+    num = arr[num_index]
+    l_num = num-1
+    r_num = num+1
+    flag = True
+    while(l_num>-1 and r_num<len(arr)):
+        left_index = arr.index(l_num)
+        right_index = arr.index(r_num)
+        if left_index < num_index and num_index < right_index:
+            flag = False
+            break
+        if right_index < num_index and num_index < left_index:
+            flag = False
+            break
+        l_num -= 1
+        r_num += 1
+    if not flag:
+        return False
+    return True
 
-
-# 0 1 2 3 4 5 6 7| 8 9 10 11 12 13 14
-# 0 1 2 3 4 5 6| 7 8 9 10 11 12 13
-# 0 1 2 3 4 5 6| 7 8 9 10 11 12
-# 0 1 2 3 4 5| 6 7 8 9 10 11
-arr = [i for i in range(10)]
-print(arr)
-recur(arr, 0, len(arr)-1)
-print(arr)
-check(arr)
+q = [i for i in range(6)]
+print(q)
+print(notDAC(q))
