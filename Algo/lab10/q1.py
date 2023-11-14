6ac5ffa173e25657840dc44b81da62dedccde439
PhasitWo <vrzoearth@gmail.com>
2023-11-15 01:48:05 +0700

    Algo Lab10 first commit

---
+++ Algo/lab10/q1.py
@@ -0,0 +1,51 @@
+import itertools, networkx, matplotlib.pyplot as plt
+
+def readInput(file_path:str) -> (list, int):
+    with open(file_path, "r") as openfile:
+        k = int(openfile.readline().strip())
+        matrix = []
+        for line in openfile:
+            row = list(map(int, line.strip().split()))
+            matrix.append(row)
+        return matrix, k
+
+def check_vertex_cover(matrix: list[list[int]], k:int) -> list:
+    vertex_cnt = len(matrix)
+    combinations = itertools.combinations(range(1, vertex_cnt+1), k)
+    ans = []
+    for c in combinations:
+        is_vc = True
+        for i in range(vertex_cnt):
+            for j in range(i+1, vertex_cnt):
+                if matrix[i][j] == 1:
+                    # plus 1 to match vertex-indexing style of combinations
+                    if (i+1 not in c) and (j+1 not in c): 
+                        is_vc = False
+                        break
+            if not is_vc:
+                break
+        if is_vc:
+            ans.append(c)
+    return ans
+
+def display(matrix: list[list[int]]) -> None:
+    g = networkx.Graph()
+    for i in range(len(matrix)):
+        for j in range(i+1, len(matrix)):
+            if matrix[i][j] == 1:
+                g.add_edge(i+1, j+1)
+    networkx.draw(g, with_labels = True)
+    print(networkx.to_numpy_array(g))
+    plt.show()
+
+
+# driver code
+if __name__ == '__main__' :
+    matrix, k = readInput("Algo/lab10/sample2.txt")
+    ans = check_vertex_cover(matrix, k)
+    display(matrix)
+    if len(ans) != 0:
+        print("Yes")
+        [print(a) for a in ans]
+    else:
+        print("No")
---
+++ Algo/lab10/q2.py
@@ -0,0 +1,44 @@
+import itertools, networkx, matplotlib.pyplot as plt
+from q1 import check_vertex_cover
+
+def readInput(file_path:str) -> (int, list):
+    with open(file_path, "r") as openfile:
+        clause_cnt = int(openfile.readline().strip())
+        clause_lst = []
+        for line in openfile:
+            row = list(map(int, line.strip().split()))
+            clause_lst.append(row)
+        return clause_cnt, clause_lst
+    
+def reduce_3SAT_to_VC(clause_lst:list[int]):
+    g = networkx.Graph()
+    # find variables
+    variables = set()
+    for clause in clause_lst:
+        x = {abs(i) for i in clause.copy()}
+        variables.update(x)
+    # create variable pair
+    for var in variables:
+        g.add_edge("vp/"+str(var), "vp/"+str(-var))
+    for index, clause in enumerate(clause_lst):
+        # create clause triangle
+        prefix = f"ct{index}/"
+        g.add_edge(prefix+str(clause[0]), prefix+str(clause[1]))
+        g.add_edge(prefix+str(clause[1]), prefix+str(clause[2]))
+        g.add_edge(prefix+str(clause[2]), prefix+str(clause[0]))
+        # connect CT and VP
+        g.add_edge(prefix+str(clause[0]), "vp/"+str(clause[0]))
+        g.add_edge(prefix+str(clause[1]), "vp/"+str(clause[1]))
+        g.add_edge(prefix+str(clause[2]), "vp/"+str(clause[2]))
+    matrix = networkx.to_numpy_array(g)
+    k = len(variables) + 2*len(clause_lst)
+    # can use this func in q1 to find solution
+    # ans = check_vertex_cover(matrix, k)
+    print(g.number_of_nodes())
+    print(k)
+    print(matrix)
+    # networkx.draw(g, with_labels = True)
+    # plt.show()
+
+clause_cnt, clause_lst = readInput("Algo/lab10/sample3.txt")
+reduce_3SAT_to_VC(clause_lst)
